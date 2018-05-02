ae629ac2c3b23f69696154be7f3781079670508e
AadityaDev <adityaverma.dev@gmail.com>
2018-05-03 00:08:35 +0530

    ride code removed

--- src/main/java/com/challenge/ridesharer/controller/RidesController.java
+++ src/main/java/com/challenge/ridesharer/controller/RidesController.java
@@ -1,106 +1,44 @@
 package com.challenge.ridesharer.controller;
 
-import java.net.URI;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.challenge.ridesharer.exception.ResourceNotFoundException;
-import com.challenge.ridesharer.model.Choice;
-import com.challenge.ridesharer.model.ChoiceVoteCount;
-import com.challenge.ridesharer.model.Poll;
-import com.challenge.ridesharer.model.User;
-import com.challenge.ridesharer.model.Vote;
-import com.challenge.ridesharer.payload.ApiResponse;
-import com.challenge.ridesharer.payload.PagedResponse;
-import com.challenge.ridesharer.payload.PollRequest;
-import com.challenge.ridesharer.payload.PollResponse;
-import com.challenge.ridesharer.security.CurrentUser;
-import com.challenge.ridesharer.security.UserPrincipal;
-import com.challenge.ridesharer.util.AppConstants;
-import com.challenge.ridesharer.util.ModelMapper;
 
 @RestController
 @RequestMapping("/api/rides")
 public class RidesController {
 
-    private static final Logger logger = LoggerFactory.getLogger(PollController.class);
+	private static final Logger logger = LoggerFactory.getLogger(RidesController.class);
 
-    @GetMapping
-    public PagedResponse<PollResponse> getPolls(@CurrentUser UserPrincipal currentUser,
-                                                @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
-                                                @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
-        return pollService.getAllPolls(currentUser, page, size);
-    }
+	// @GetMapping
+	// public PagedResponse<RideDetail> getPolls(@CurrentUser UserPrincipal
+	// currentUser,
+	// @RequestParam(value = "page", defaultValue =
+	// AppConstants.DEFAULT_PAGE_NUMBER) int page,
+	// @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE)
+	// int size) {
+	// }
 
-    @PostMapping
-    @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<?> createPoll(@Valid @RequestBody PollRequest pollRequest) {
-        Poll poll = new Poll();
-        poll.setQuestion(pollRequest.getQuestion());
+	// @PostMapping
+	// @PreAuthorize("hasRole('USER')")
+	// public ResponseEntity<?> startRide(@Valid @RequestBody PollRequest
+	// pollRequest) {
+	//
+	// }
 
-        pollRequest.getChoices().forEach(choiceRequest -> {
-            poll.addChoice(new Choice(choiceRequest.getText()));
-        });
+	// @PostMapping
+	// @PreAuthorize("hasRole('USER')")
+	// public ResponseEntity<?> stopRide(@Valid @RequestBody PollRequest
+	// pollRequest) {
+	//
+	// }
 
-        Instant now = Instant.now();
-        Instant expirationDateTime = now.plus(Duration.ofDays(pollRequest.getPollLength().getDays()))
-                .plus(Duration.ofHours(pollRequest.getPollLength().getHours()));
+	//
+	//
+	// @GetMapping("/{rideId}")
+	// public PollResponse getPollById(@CurrentUser UserPrincipal currentUser,
+	// @PathVariable Long pollId) {
+	// }
 
-        poll.setExpirationDateTime(expirationDateTime);
-
-        Poll result = pollRepository.save(poll);
-
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest().path("/{pollId}")
-                .buildAndExpand(result.getId()).toUri();
-
-        return ResponseEntity.created(location)
-                .body(new ApiResponse(true, "Poll Created Successfully"));
-    }
-
-
-    @GetMapping("/{pollId}")
-    public PollResponse getPollById(@CurrentUser UserPrincipal currentUser,
-                                    @PathVariable Long pollId) {
-        Poll poll = pollRepository.findById(pollId).orElseThrow(
-                () -> new ResourceNotFoundException("Poll", "id", pollId));
-
-        // Retrieve Vote Counts of every choice belonging to the current poll
-        List<ChoiceVoteCount> votes = voteRepository.countByPollIdGroupByChoiceId(pollId);
-
-        Map<Long, Long> choiceVotesMap = votes.stream()
-                .collect(Collectors.toMap(ChoiceVoteCount::getChoiceId, ChoiceVoteCount::getVoteCount));
-
-        // Retrieve poll creator details
-        User creator = userRepository.findById(poll.getCreatedBy())
-                .orElseThrow(() -> new ResourceNotFoundException("User", "id", poll.getCreatedBy()));
-
-        // Retrieve vote done by logged in user
-        Vote userVote = null;
-        if(currentUser != null) {
-            userVote = voteRepository.findByUserIdAndPollId(currentUser.getId(), pollId);
-        }
-
-        return ModelMapper.mapPollToPollResponse(poll, choiceVotesMap,
-                creator, userVote != null ? userVote.getChoice().getId(): null);
-    }
-	
 }
